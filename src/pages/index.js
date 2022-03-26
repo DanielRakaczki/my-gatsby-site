a34568c38bb4663299bf01587d8c4158070a00af
danielrakaczki <daniel.rakaczki@gmail.com>
2022-03-26 12:38:54 +0100

    Added layout and style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <Link to="/about">About page</Link>
-    </main>
+    <Layout pageTitle="Home page">
+      <p>I'm making this by following the Gatsby tutorial!</p>
+    </Layout>
   )
 }
 
